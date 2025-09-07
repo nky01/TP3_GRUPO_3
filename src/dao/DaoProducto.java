a595b26a260b591beb853726bdfc09812e66d508
Nicole <nickybelu@gmail.com>
2025-09-07 17:29:08 -0300

    punto d

--- src/dao/DaoProducto.java
+++ src/dao/DaoProducto.java
@@ -1,5 +1,33 @@
 package dao;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DaoProducto {
 
+	private String host = "jdbc:mysql://localhost:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String dbName = "bdInventario";
+	
+	public DaoProducto() {}
+	
+	public void ejecutarProcedimientoAlmacenado(Producto producto) {
+		Connection cn = null;
+		try {
+			cn = DriverManager.getConnection(host+dbName, user, pass);
+			CallableStatement cst = cn.prepareCall("CALL sp_AgregarProducto(?, ?, ?, ?, ?)");
+			cst.setString(1, producto.getCodigo());
+			cst.setString(2, producto.getNombre());
+			cst.setBigDecimal(3, producto.getPrecio());
+			cst.setString(4, producto.getStock());
+			cst.setString(5, producto.getIdCategoria());
+			cst.execute();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
