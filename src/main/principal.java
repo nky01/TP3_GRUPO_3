a288a2b1fc4512e80ac71a4ac23e6094b9cc3cc3
Nicole <nickybelu@gmail.com>
2025-09-09 19:17:28 -0300

    5/10 productos

--- src/main/principal.java
+++ src/main/principal.java
@@ -9,9 +9,16 @@ public class principal {
 	public static void main(String[] args) {
 		
 		DaoProducto dp = new DaoProducto();
-		Producto pr = new Producto("COD-001", "Producto1", BigDecimal.valueOf(125.002), 2, 3);
-		dp.ejecutarProcedimientoAlmacenado(pr);
-		
+		Producto[] productos = new Producto[] {
+	            new Producto("COD-001", "Producto1", BigDecimal.valueOf(125.002), 2, 3),
+	            new Producto("COD-002", "Producto2", BigDecimal.valueOf(230.50), 15, 2),
+	            new Producto("COD-003", "Producto3", BigDecimal.valueOf(75.99), 20, 3),
+	            new Producto("COD-004", "Producto4", BigDecimal.valueOf(300.00), 5, 1),
+	            new Producto("COD-005", "Producto5", BigDecimal.valueOf(150.75), 12, 2)
+		};
+		for (Producto p : productos) {
+	            dp.ejecutarProcedimientoAlmacenado(p);
+		}
+		System.out.println("Los productos se cargaron correctamente!");	
 	}
-
 }
